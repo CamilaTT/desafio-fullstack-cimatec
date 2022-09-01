fe4178f95459211202a641537f6b7fe1d5742ba0
Camila <camila99pessoa@gmail.com>
2022-08-31 23:25:14 -0300

    updating vehicle table

--- api/src/app/infra/vehicleTable.js
+++ api/src/app/infra/vehicleTable.js
@@ -26,6 +26,8 @@ class vehicleTable {
         this.insertVehicle('Mustang', 1500, 500, 750);
         this.insertVehicle('Territory', 4560, 4000, 3050);
         this.insertVehicle('Bronco Sport', 7560, 4060, 2050);
+        this.insertVehicle('Fusion', 29000, 20000, 17000);
+        this.insertVehicle('Maverick', 40000, 30000, 28150);
     }
 
     insertVehicle(vehicle, totalSales, connected, softwareUpdates) {
